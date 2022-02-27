572b71e8599c586be7a88ee005721a2e67d5281c
dopeks <kyusang.dev@gmail.com>
2022-02-27 17:21:59 +0900

    Initial commit

---
+++ src/main/java/com/treepet/mvptest/MvpTestApplication.java
@@ -0,0 +1,13 @@
+package com.treepet.mvptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvpTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/treepet/mvptest/MvpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treepet.mvptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
