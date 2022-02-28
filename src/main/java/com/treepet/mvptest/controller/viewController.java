24fff836508ac58538117adaacf0a59f6b7ff8eb
dopeks <kyusang.dev@gmail.com>
2022-02-28 09:37:03 +0900

    basic view setting

---
+++ src/main/java/com/treepet/mvptest/controller/viewController.java
@@ -0,0 +1,16 @@
+package com.treepet.mvptest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class viewController {
+
+    // main
+    @GetMapping(value = "")
+    public String Home(Model model){
+        return "main";
+    }
+
+}
