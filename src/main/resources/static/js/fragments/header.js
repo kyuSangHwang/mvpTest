bf6291ef257045e6c37f0b82fd937958706f7df6
dopeks <kyusang.dev@gmail.com>
2022-03-02 18:07:45 +0900

    header 완성

--- src/main/resources/static/js/fragments/header.js
+++ src/main/resources/static/js/fragments/header.js
@@ -1,64 +1,30 @@
-let mobileStatusHeader = false;
-let nowPath = "";
 
 $(document).ready(function () {
-    const filter = "win16|win32|win64|mac|macintel";
-    const webType = "";
 
-    if (navigator.platform) {
-        if (0 > filter.indexOf(navigator.platform.toLowerCase())) {
-            //mobile
-            mobileStatusHeader = true;
-        } else {
-            //pc
-            mobileStatusHeader = false;
-            headerManager();
-        }
-    }
 });
 
-function headerManager() {
-    const $headerWrap = $('#headerWrap');
-    const $headerLogo = $('.companyLogo');
-    let pathName = location.pathname;
-
-    if (pathName != "/") {
-        $headerWrap.css('position', 'absolute');
-    } else {
-        $headerWrap.css('position', 'fixed');
-        $headerWrap.css('color', 'white');
-        $headerWrap.css('background-color', 'rgba(0,0,0,0)');
-        $headerLogo.css('background-image', 'url("/images/header/sampleLogo.png")');
-    }
+// TREEPET
+function treePetInfo() {
+    locationSearch();
+    location.href = "/treePetInfo" + locationSearch();
 }
 
-function mainHeaderManager(index) {
-    if (mobileStatusHeader) {
-        const menu = $('#menu');
-        if (index > 0) {
-            menu.css("color", "black");
-        } else {
-            menu.css("color", "white");
-        }
-    } else {
-        const $headerWrap = $('#headerWrap');
-        const $headerLogo = $('.companyLogo');
-        console.log(index);
-
-        if (index > 0) {
-            $headerWrap.css('color', 'black');
-            $headerLogo.css('background-image', 'url("/images/header/sampleLogo.png")');
-        } else {
-            $headerWrap.css('color', 'white');
-            $headerWrap.css('background-color', 'rgba(0,0,0,0)');
-            $headerLogo.css('background-image', 'url("/images/header/sampleLogo.png")');
-        }
-    }
-}
-
+// 회사소개
 function companyInfo() {
     locationSearch();
     location.href = "/companyInfo" + locationSearch();
+}
+
+// 이용방법
+function howToUseInfo() {
+    locationSearch();
+    location.href = "/howToUseInfo" + locationSearch();
+}
+
+// Contact Us
+function contactUsInfo() {
+    locationSearch();
+    location.href = "/contactUsInfo" + locationSearch();
 }
 
 function locationSearch() {
