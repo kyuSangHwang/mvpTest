847d42a3db660a800e05c9fc50696e9e51c97ff1
dopeks <kyusang.dev@gmail.com>
2022-04-29 00:43:09 +0900

    JDK 변경 및 SpringBootServletInitializer 상속

--- src/main/java/com/treepet/mvptest/MvpTestApplication.java
+++ src/main/java/com/treepet/mvptest/MvpTestApplication.java
@@ -2,9 +2,10 @@ package com.treepet.mvptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvpTestApplication {
+public class MvpTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MvpTestApplication.class, args);
